f2a441e7db9bc1ea7af5e79f8e239ce8d070c405
Nikita Polivanchuk <ipz231_pnr@student.ztu.edu.ua>
2023-11-20 00:23:06 +0200

    Adding project PointsAverage

---
+++ PointsAverage/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] points;
+
+        Console.WriteLine("Enter student's points:");
+        while (true)
+        {
+            try
+            {
+                Console.Write(">> ");
+                points = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+                break;
+            }
+            catch
+            {
+                Console.WriteLine("Invalid input data");
+            }
+        }
+        Console.WriteLine("Student is {0}admitted to the exam", (points.Average() >= 50) ? "" : "not ");
+    }
+}
